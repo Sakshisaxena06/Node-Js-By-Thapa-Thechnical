718bfdc182dff4dab4a401308cea06d9570c5157
Sakshisaxena06 <saxenasakshi325@gmail.com>
2026-02-21 18:21:01 +0530

    Weather by node js

--- NodePrac/mini_project/Weather.js
+++ NodePrac/mini_project/Weather.js
@@ -6,3 +6,32 @@ const rl = readline.createInterface({
 
 })
 
+const apikey='1284aef5c78c4d0b364775b5a545220a';
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather'
+
+   const city =  rl.question("Enter the city name :" , async (city) => {
+         console.log("Weather Information :");
+         const url = `${baseUrl}?q=${city}&appid=${apikey}`;
+ 
+try {
+    const respone = await fetch(url);
+    const data = await respone.json();
+    console.log(data);
+    
+    console.log("City : ", data.name);
+    console.log("Temperature : ", data.main.temp);
+    console.log("Humidity :  ", data.main.humidity);
+    console.log("Description : ", data.weather[0].description);
+    console.log("Wind Speed : ", data.wind.speed);
+    rl.close();
+    
+
+} catch (error) {
+    console.log(error.message);
+    
+}
+});
+
+
+
+    
